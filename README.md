9027acc9642ee6a373dac215bdf7bfcb311abcc9
Radu Ioan Fericean <radu@fericean.ro>
2012-08-26 17:23:40 +0300

    better configuration path and added goci.me continous integration

--- README.md
+++ README.md
@@ -2,3 +2,23 @@ gmail2go
 ========
 
 Simple gmail multiple account mail notification command line
+
+Usage
+-----
+
+rif@grace:~$ gmail2go -help
+Usage of gmail2go:
+  -accounts-file="/home/rif/.gmail2gorc": the user account file
+  -set="": adds/updates/deletes to user:password to the accounts file (leave password empty to delete)
+
+gmail2go -set user:secret - to create an account
+
+gmail2go -set user:changed_secret - to update an account
+
+gmail2go -set user: - to delete an account
+
+Example: run it every 5 minutes with sound notification
+
+watch -n 600 "gmail2go && play -q /usr/share/sounds/gnome/default/alerts/drip.ogg"
+
+Continous integration: [![Build Status](https://goci.herokuapp.com/project/image/github.com/rif/gmail2go "Continous integration")](http://goci.me/project/github.com/rif/gmail2go)
--- gmail2go.go
+++ gmail2go.go
@@ -10,59 +10,62 @@ import (
 	"github.com/rif/gmail2go/rss"
 	"log"
 	"os"
+	"path"
 	"strings"
 )
 
 var (
-	accountFile = flag.String("account-file", ".gmail2gorc", "the user account filee to be used")
-	set         = flag.String("set", "", "adds/updates/deletes au user:password to the accounts file")
-	accountsMap = make(map[string]string)
+	accountsFile = flag.String("accounts-file", path.Join(os.Getenv("HOME"), ".gmail2gorc"), "the user accounts file")
+	set          = flag.String("set", "", "adds/updates/deletes user:password to the accounts file (leave password empty to delete)")
+	accountsMap  = make(map[string]string)
 )
 
 func main() {
 	flag.Parse()
-	fin, err := os.Open(*accountFile)
-	if err != nil {
-		if *set != "" {
-			fin, err = os.Create(*accountFile)
-			defer fin.Close()
-			w := bufio.NewWriter(fin)
-			up := strings.SplitN(*set, ":", 2)
-			if len(up) != 2 {
-				log.Fatal("Incorrect set string use user:pass")
-			}
-			accountsMap[up[0]] = up[1]
-			var out bytes.Buffer
-			enc := json.NewEncoder(&out)
-			err := enc.Encode(accountsMap)
-			if err != nil {
-				log.Fatal("Could not encode accounts to json", err)
-			}
-			err = passwd.Encrypt(w, &out, make([]byte, 16), make([]byte, 16))
-			if err != nil {
-				log.Fatal("Could not encrypt accounts json string to file", err)
-			}
-		}
-		if err != nil {
-			log.Fatal("Cannot open account file: ", err)
-		}
-	} else {
-
+	fin, err := os.Open(*accountsFile)
+	if err == nil {
 		r := bufio.NewReader(fin)
 		res, err := passwd.Decrypt(r, make([]byte, 16), make([]byte, 16))
 		if err != nil {
 			log.Fatal("Could not decrypt accounts file: ", err)
-		}
-		dec := json.NewDecoder(res)
-		if err := dec.Decode(&accountsMap); err != nil {
-			log.Fatal("Could not decode accounts content to json: ", err)
+		} else {
+			dec := json.NewDecoder(res)
+			if err := dec.Decode(&accountsMap); err != nil {
+				log.Fatal("Could not decode accounts content to json: ", err)
+			}
 		}
 	}
-
 	if *set != "" {
-		//
+		fin, err = os.Create(*accountsFile)
+		if err != nil {
+			log.Fatal("Cannot open account file: ", err)
+		}
+		defer fin.Close()
+		w := bufio.NewWriter(fin)
+		up := strings.SplitN(*set, ":", 2)
+		if len(up) != 2 {
+			log.Fatal("Incorrect set string use user:pass")
+		}
+		if up[1] != "" {
+			accountsMap[up[0]] = up[1]
+		} else {
+			delete(accountsMap, up[0])
+		}
+		var out bytes.Buffer
+		enc := json.NewEncoder(&out)
+		err := enc.Encode(accountsMap)
+		if err != nil {
+			log.Fatal("Could not encode accounts to json", err)
+		} else {
+			err = passwd.Encrypt(w, &out, make([]byte, 16), make([]byte, 16))
+			if err != nil {
+				log.Fatal("Could not encrypt accounts json string to file", err)
+			}
+			w.Flush()
+		}
 	}
 
+	foundAtLeastOne := false
 	for user, pass := range accountsMap {
 		fmt.Println("Account: ", user)
 		mails, err := rss.Read("https://mail.google.com/mail/feed/atom", user, pass)
@@ -71,6 +74,14 @@ func main() {
 		}
 		for _, m := range mails {
 			fmt.Println("\t", m.Title)
+			foundAtLeastOne = true
+		}
+		if len(mails) == 0 {
+			fmt.Println("\tNo unread email.")
 		}
 	}
+	if foundAtLeastOne {
+		os.Exit(0)
+	}
+	os.Exit(1)
 }
